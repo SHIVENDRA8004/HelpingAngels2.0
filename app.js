2dcfcbb78fa20f1d46b34971ae920537f4ddda09
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-08 21:49:45 +0530

    Morgan used

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const morgan = require('morgan');
+const morgan = require("morgan");
 const app = express();
 app.listen(3000, () => {
   console.log("Server Started on port 3000");
 });
-app
+app.use(morgan("dev"));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
